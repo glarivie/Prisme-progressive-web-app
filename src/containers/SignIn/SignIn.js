fb05d14520742620c22867bca5acfb96e2722bdc
Guillaume LARIVIERE <glarivie@e2r1p14.42.fr>
2017-01-19 18:48:44 +0100

    WIP

---
+++ src/containers/SignIn/SignIn.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import FontAwesome from 'react-fontawesome';
+
+import styles from './SignIn.css';
+
+class SignIn extends Component {
+  state = {
+    mail: '',
+    password: '',
+  };
+
+  handleChange = (value, type) => {
+    this.setState({ [type]: value });
+  };
+
+  handleSubmit = e => e.preventDefault();
+
+  render() {
+    return (
+      <div className={styles.signin}>
+        <form onSubmit={this.handleSubmit}>
+          <div className={styles.group}>
+            <FontAwesome name="envelope-o" className={styles.icon} />
+            <input
+              autoComplete="off"
+              name="mail"
+              type="email"
+              onChange={({ target: { value } }) => this.handleChange(value, 'mail')}
+              placeholder="Entrez votre adresse e-mail"
+            />
+          </div>
+          <div className={styles.group}>
+            <FontAwesome name="lock" className={styles.icon} />
+            <input
+              autoComplete="new-password"
+              name="password"
+              type="password"
+              onChange={({ target: { value } }) => this.handleChange(value, 'password')}
+              placeholder="Entrez votre mot de passe"
+            />
+          </div>
+          <button type="submit">Connexion</button>
+          <small>Pas encore de compte ? <Link to="/signup">Inscrivez-vous</Link></small>
+        </form>
+        <footer>
+          &copy; Copyright Gazett 2017
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
--- src/router.js
+++ src/router.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Route, IndexRoute, Redirect } from 'react-router';
 
 import App from './containers/App';
-// import Home from './containers/Home';
+import SignIn from './containers/SignIn';
 // import Page404 from './containers/Page404';
 
 export default (
   <Route path="/" component={App}>
-    {/* <IndexRoute component={Home} /> */}
+    <IndexRoute component={SignIn} />
     {/* <Route path="404" component={Page404} /> */}
     {/* <Redirect from="*" to="404" /> */}
   </Route>
