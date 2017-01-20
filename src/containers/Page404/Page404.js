af366d51fbc8b13b20db34dfa91bab75e69e61ab
Guillaume LARIVIERE <glarivie@e2r3p7.42.fr>
2017-01-20 12:45:05 +0100

    page 404

---
+++ src/containers/Page404/Page404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Page404.css';
+
+const Page404 = () => (
+  <div className={styles.page404}>
+    <h2>Page introuvable</h2>
+  </div>
+);
+
+export default Page404;
--- src/router.js
+++ src/router.js
@@ -3,12 +3,12 @@ import { Route, IndexRoute, Redirect } from 'react-router';
 
 import App from './containers/App';
 import SignIn from './containers/SignIn';
-// import Page404 from './containers/Page404';
+import Page404 from './containers/Page404';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={SignIn} />
-    {/* <Route path="404" component={Page404} /> */}
-    {/* <Redirect from="*" to="404" /> */}
+    <Route path="404" component={Page404} />
+    <Redirect from="*" to="404" />
   </Route>
 );
