e8099953890c2b01d194c61721fba2c2ed514a81
Guillaume LARIVIERE <glarivie@e2r3p7.42.fr>
2017-01-20 13:11:17 +0100

    WIP homepage

--- src/router.js
+++ src/router.js
@@ -3,11 +3,14 @@ import { Route, IndexRoute, Redirect } from 'react-router';
 
 import App from './containers/App';
 import SignIn from './containers/SignIn';
+import HomePage from './containers/HomePage';
 import Page404 from './containers/Page404';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={SignIn} />
+    <Route path="sign-in" component={SignIn} />
+    <Route path="home" component={HomePage} />
     <Route path="404" component={Page404} />
     <Redirect from="*" to="404" />
   </Route>
