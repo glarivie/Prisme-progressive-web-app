0fe56e60d3af076d734997015b2a30e392dbfed1
Manon KANTZER <mkantzer@e2r4p13.42.fr>
2017-01-25 11:43:34 +0100

    Added data prism2 + logo pic

---
+++ src/constants/prism2.js
@@ -0,0 +1,101 @@
+import React from 'react';
+
+const prisme2 = [{
+    title: "Fessenheim : \"C'est une grosse opération d'enfumage\", dénonce Jadot",
+    img: '../assets/prism/p2-1-1.png',
+    author: 'Marc-Olivier Fogiel, Claire Gaveau',
+    category: 'Environnement',
+    source: 'RTL',
+    content:
+      <article>
+        <p>INVITÉ RTL - Le candidat écologiste à la présidentielle de 2017 ne croit pas à la fermeture de la centrale malgré le dernier accord annoncé entre l'État et EDF ce mardi 24 janvier.</p>
+        <p>Une enveloppe de 446 millions d'euros pour compenser le manque à gagner et la suppression de 2.000 emplois. Le Conseil d'administration d'EDF a accepté l'accord d'indemnisation proposé par l'État afin d'enfin réussir à fermer la centrale de Fessenheim. Mais le chemin est encore long. Et Yannick Jadot, candidat écologiste à la présidentielle 2017 croit "de moins en moins" à la fermeture définitivement de la plus vieille centrale française alors que c'était une promesse du quinquennat de François Hollande.</p>
+        <p>Invité de RTL, Yannick Jadot dénonce "une grosse opération d'enfumage". "On fait le lien entre le démarrage de l'EPR de Flamanville et la fermeture de Fessenheim, c'est-à-dire qu'on ne fera pas ça avant le changement de présidence. Donc, on laisse cela entre les mains du prochain gouvernement et c'est totalement irresponsable", a-t-il déploré regrettant qu'EDF soit "prêt à accepter beaucoup d'argent" tout en "reculant au maximum toutes décisions en matière de fermeture". Yannick Jadot pointe dès lors les problèmes de sécurité en jeu rappelant qu'un des réacteurs était éteint depuis plus de 200 jours.</p>
+        <p>L'avenir de la centrale est donc cantonné à l'élection présidentielle de mai prochain. Malgré le programme tourné vers l'écologie, Yannick Jadot refuse de soutenir l'ancien ministre de l'Éducation nationale. "Pourquoi voulez-vous que je me rallier à Benoît Hamon qui après doit se rallier à Mélenchon, qui va se rallier à Macron, qui va se rallier à Fillon ?, a-t-il questionné. Je suis candidat écologiste. On verra ce qu'il se passera dans l'avenir". Une manière pour le candidat de critiquer la politique "verte" menée par le gouvernement tout au long du quinquennat. "Ce gouvernement, avec le Parti socialiste, n'a pas voulu tourner la page du nucléaire", explique-t-il saluant tout de même la mise en avant de certaines idées dans le débat public.</p>
+      </article>
+    }, {
+      title: "À Fessenheim, Sarkozy tire à boulets rouges contre le projet de fermeture du site",
+      img: '../assets/logo/figaro.png',
+      author: 'Charles Jaigu',
+      category: 'Environnement',
+      source: 'Figaro',
+      content:
+      <article>
+        <p>À l'occasion d'un déplacement à Belfort, le patron de l'UMP a fait une visite surprise sur le site de la centrale nucléaire.</p>
+        <p>D'humeur batailleuse, Nicolas Sarkozy est arrivé à 14 heures sur le site de Fessenheim (Haut-Rhin). Très attendu par les salariés de la centrale nucléaire, mais aussi par les élus UMP de la ville et de la circonscription. Il a immédiatement ouvert le feu contre François Hollande : « Je n'ai pas l'intention de laisser sacrifier Fessenheim sur l'autel de la reconstitution de la majorité plurielle », a-t-il lancé dans la petite salle de conférence où une cinquantaine de salariés étaient venus l'écouter. Une allusion aux efforts fournis par l'Élysée ces derniers temps pour rétablir les relations avec les ex-alliés d'EELV.</p>
+        <p>« Il y en a qui vont à Florange pour renier leurs promesses, moi je vais à Fessenheim pour honorer les miennes », a-t-il proclamé. « Je vous trouve assez calmes, compte tenu du contexte invraisemblable de mensonge, d'incompétence et de malhonnêteté ». Un véritable festival où Nicolas Sarkozy a rappelé les raisons pour lesquelles il refusait la fermeture du site : « Fessenheim est aux normes de sécurité, elle est amortie, et gagne de l'argent, nous n'accepterons pas le fait accompli », a-t-il lancé en prenant les accents d'un leader syndical.</p>
+        <p>« Ça paraît très simple quand je le dis, mais mon successeur ne comprend pas, il doit y avoir une erreur quelque part » Nicolas Sarkozy</p>
+        <p>Il a rappelé que Fessenheim était déjà l'un des sujets d'opposition entre lui et François Hollande pendant la campagne présidentielle de 2012. « Je l'avais dit aux syndicats dont toutes les directions nationales ont appelé à voter François Hollande ». Le président de l'UMP s'est délecté de l'interpellation du conseiller général de Fessenheim : « Vous les connaissez bien les pastèques qui défilent partout, elles sont rouges à l'intérieur et vertes autour, des aigris de la société, monsieur le président, mettez-moi de l'ordre dans tout ça », a-t-il lancé en rappelant que « 2000 emplois sont en jeu, et 800 personnes qui vivent à Fessenheim risquent de partir ».</p>
+        <p>Nicolas Sarkozy a aussi ironisé sur le délégué ministériel à la fermeture de Fessenheim qui a été désigné récemment par le gouvernement. « Encore un chômeur de plus », a lâché Sarkozy. « Il n'y a pas un Français qui ignore ce qu'il faut penser des promesses de M. Hollande », a-t-il expliqué en laissant entendre que jamais le gouvernement ne pourrait « fermer le site dans les deux ans qui viennent. Où vont-ils trouver l'argent pour démanteler le site ? Cela va coûter cinq milliards d'euros. » Nicolas Sarkozy s'est également dit favorable à une nouvelle génération d'EPR : « Si nous arrêtons la recherche, nous arrêtons la filière ». Décidément remonté, le patron de l'UMP a conclu son propos en visant, encore une fois, le président de la République : « Ça paraît très simple quand je le dis, mais mon successeur ne comprend pas, il doit y avoir une erreur quelque part ».</p>
+      </article>
+    }, {
+      title: "Fessenheim : EDF ouvre la voie à l’arrêt de la centrale nucléaire",
+      img: 'http://s2.lemde.fr/image/2017/01/24/534x0/5068218_6_d308_francois-hollande-s-etait-engage-a-fermer_1542e180a6e07670f842526acac5eff3.jpg',
+      author: 'Jean-Michel Bezat',
+      category: 'Environnement',
+      source: 'Monde',
+      content:
+      <article>
+        <p>Le conseil d’administration du groupe a donné son feu vert à l’accord d’indemnisation pour une fermeture anticipée de la plus vieille centrale de France.</p>
+        <p>Une nouvelle étape importante a été franchie, mardi 24 janvier, dans le règlement du « dossier Fessenheim » qui traîne depuis près de cinq ans. Au terme d’un compromis avec l’Etat, le conseil d’administration d’Electricité de France (EDF) a voté de justesse – grâce à la voix prépondérante de son PDG, Jean-Bernard Lévy – le mécanisme d’indemnisation du groupe d’électricité et de ses partenaires allemands et suisses pour l’arrêt de la centrale du Haut-Rhin. L’indemnisation sera constituée d’une part fixe de 490 millions d’euros versés d’ici à 2021 (coûts de reconversion du personnel, démantèlement…) et d’une part variable pour compenser d’éventuels manques à gagner jusqu’en 2041.</p>
+        <p>La ministre de l’environnement et de l’énergie a immédiatement salué une « décision équilibrée (…) qui va permettre de nouveaux investissements industriels franco-allemands sur le territoire du Haut-Rhin », indiquant qu’elle avait contacté le ministre allemand de l’économie, Sigmar Gabriel. Parmi les pistes de reconversion, Ségolène Royal a évoqué l’éventuelle installation d’une usine de voitures électriques Tesla, un projet d’usine de batteries de troisième génération ou la création d’un site pilote de démantèlement de centrales.</p>
+        <p>« C’est aussi un choix positif pour l’entreprise », a estimé Mme Royal. EDF pourra « rationaliser » ses investissements, les réorienter vers les énergies renouvelables et « respecter la loi de transition énergétique » qui prévoit de ramener de 75 % à 50 % la part du nucléaire dans la production d’électricité en France.</p>
+        <p>Le vote était loin d’être acquis il y a seulement quelques jours. Le conseil d’administration d’EDF compte dix-huit membres. Le gouvernement a travaillé au corps les six administrateurs indépendants, seuls susceptibles de voter en faveur d’un arrêt de la centrale : M. Lévy, dont le vote favorable était acquis ; Laurence Parisot, ex-présidente du Medef ; Bruno Lafont, ancien patron de Lafarge ; Colette Lewiner, conseillère du président de Capgemini ; Philippe Crouzet, président du directoire de Vallourec ; Claire Pedini, directrice générale adjointe de Saint-Gobain.</p>
+        <h3>Première étape vers une fermeture</h3>
+        <p>Leur accord était en effet nécessaire. Les six représentants de l’Etat ne pouvaient pas prendre part au vote sans être accusés de conflit d’intérêt. Quant aux six administrateurs salariés (CGT, CFDT, FO et CFE-CGC), ils avaient annoncé depuis des semaines qu’ils se prononceraient contre une décision contraire, selon eux, aux intérêts des salariés, de l’entreprise et de l’activité autour d’une centrale qui génère 2 200 emplois, dont la moitié dans la centrale elle-même avec ses 850 salariés d’EDF et ses 250 prestataires permanents.</p>
+        <p>En cas de vote négatif sur Fessenheim, les pouvoirs publics menaçaient EDF de ne pas prendre deux textes réglementaires : le premier pour permettre la poursuite du chantier de Flamanville ; le second pour relancer le réacteur numéro 2 de la centrale nucléaire de Paluel (Seine-Maritime), en arrêt prolongé à la suite de la chute d’un générateur de vapeur de 450 tonnes dans le bâtiment réacteur.</p>
+        <p>La Commission européenne devra se prononcer sur le protocole d’indemnisation au regard du respect des règles sur les aides d’Etat. La voie sera alors libre pour que le gouvernement prenne, avant l’élection présidentielle d’avril-mai 2017, le décret d’abrogation de l’autorisation d’exploiter la doyenne du parc nucléaire français. Ainsi François Hollande pourra-t-il dire qu’il a au moins enclenché le processus, sans avoir respecté la promesse faite à ses alliés d’Europe Ecologie-Les Verts en 2011 de fermer la centrale au cours de son quinquennat.</p>
+        <h3>Des indemnités jusqu’en 2041</h3>
+        <p>L’indemnisation devra notamment compenser les pertes de recettes de ces deux réacteurs nucléaires de 900 mégawatts (MW) que l’Autorité de sûreté nucléaire (ASN) avait pourtant autorisé à fonctionner pendant encore six ans. Un tiers sera versé au partenaire allemand EnBW et à un consortium suisse, qui ont financé ensemble 32,5 % de l’installation et « tirent » un tiers du courant produit.</p>
+        <p>Les deux réacteurs ne seront pas arrêtés avant la fin de 2018, l’horizon prévu par EDF pour le raccordement au réseau de l’EPR de Flamanville. Ses 1 650 MW compenseront alors les capacités électro-nucléaires perdues à Fessenheim, et la France conservera la même capacité de 63 000 MW de nucléaire, sur un parc de production de 130 000 MW (nucléaire, hydraulique, thermique, renouvelables).</p>
+        <h3>Débrayages appelés par les syndicats</h3>
+        <p>Les syndicats de la centrale ont appelé à des débrayages depuis lundi, soulignant le gâchis économique et social entraîné par une telle décision. Fessenheim, qui produit 2,4 % du courant français, a été la première centrale mise aux nouvelles normes post-Fukushima imposées par l’ASN. Elle génère plusieurs dizaines de millions d’euros de recettes, même si EDF se refuse à fournir des données sur la rentabilité de cette usine.</p>
+        <p>Tout en dénonçant les retards, les écologistes français, rejoints par des Verts allemands et suisses des régions frontalières, applaudiront à la fermeture d’une centrale qu’ils jugent « dangereuse », non seulement en raison de son ancienneté, mais aussi de sa situation en zone inondable et sur une faille sismique.</p>
+        <p>Yannick Jadot en a fait un symbole : le candidat désigné d’Europe Ecologie-Les Verts à l’élection présidentielle s’est rendu sur le site alsacien, le 20 janvier. Il a plaidé pour des « plans de conversion industrielle et professionnelle » afin d’éviter les pertes d’emplois. Ancien responsable des campagnes antinucléaires de Greenpeace, M. Jadot défend une « sortie progressive et définitive du nucléaire d’ici à 2035 ». Fessenheim doit être, selon lui, une des toutes premières centrales à fermer dès 2017.</p>
+        <p>Le feuilleton politique n’en est pas terminé pour autant. Car ce qu’un décret gouvernemental peut faire, un autre peut le défaire. François Fillon, candidat de la droite à l’élection présidentielle, s’est engagé à « stopper la fermeture » de la centrale s’il est élu en mai. Une centrale qui a reçu une autorisation de l’ASN pour produire jusqu’au 31 décembre 2022.</p>
+      </article>
+    }, {
+      title: "Fessenheim : une fermeture sans cesse repoussée",
+      img: '../assets/logo/francetv.png',
+      author: 'Louis San',
+      category: 'Environnement',
+      source: 'Francetv',
+      content:
+      <article>
+        <p>Franceinfo revient sur la promesse de campagne de François Hollande, qui a été plusieurs fois contrariée durant son quinquennat. La date exacte de la fermeture de la centrale nucléaire reste incertaine, mais elle pourrait intervenir fin 2018.</p>
+        <p>Le conseil d'administration d'EDF a adopté, mardi 24 janvier, un accord qui ouvre la voie à l'arrêt effectif de la centrale nucléaire de Fessenheim (Haut-Rhin). La fermeture de la plus vieille centrale nucléaire de France, qui était une promesse du candidat Hollande en 2011, a été plusieurs fois repoussée pendant son quinquennat.</p>
+        <p>"La centrale de Fessenheim, qui est la plus ancienne de notre parc, sera fermée à la fin de l’année 2016", avait-il assuré, en septembre 2012, en inaugurant la conférence environnementale. Philippe Martin, alors ministre de l'Ecologie, a confirmé le calendrier dans une interview au Journal du dimanche, en juillet 2013. "J’ai un mandat : je fermerai Fessenheim d’ici au 31 décembre 2016."</p>
+        <h3>2014 : Royal sème le trouble</h3>
+        <p>Mais à l'automne 2014, Ségolène Royal, qui a remplacé entre-temps Philippe Martin au ministère (renommé "ministère de l'Environnement"), sème le trouble. Elle laisse entendre que d'autres réacteurs que Fessenheim pourraient fermer.</p>
+        <p>"Lorsque le réacteur de Flamanville va ouvrir, normalement en 2016, deux autres réacteurs devront  fermer. Soit les plus anciens, soit ceux qui nécessiteraient l'investissement le plus coûteux en  termes de remise aux normes (...). Cela pourrait être d'autres réacteurs que Fessenheim (...)", déclare-t-elle sur RTL.</p>
+        <h3>La fermeture prévue en 2018</h3>
+        <p>Moins d’un an plus tard, en septembre 2015, la ministre rétropédale. Elle évoque une possible fermeture, mais en 2018. "Pour fermer deux réacteurs comme ceux de Fessenheim, il ne suffit pas de tourner un bouton", lance-t-elle sur RTL.</p>
+        <p>François Hollande confirme ensuite, dans un entretien au Parisien Magazine, l’échéance de 2018. Le chef de l'Etat confirme également le lien entre la fermeture de Fessenheim et le lancement de l’EPR de Flamanville (Manche), prévu en 2018.</p>
+        <h3>L'EPR de Flamanville livré fin 2018 ?</h3>
+        <p>Le 28 octobre 2016, avec plusieurs mois de retard, le gouvernement publie sa feuille de route de transition énergétique (document PDF), la Programmation pluriannuelle de l'énergie (PPE). La loi sur la transition énergétique, dont la PPE est le guide, prévoit de passer de 75% à 50% d'électricité d'origine nucléaire en 2025. La fermeture de la centrale de Fessenheim y est clairement évoquée à l'horizon 2018.</p>
+        <p>Reste à savoir si l'EPR de Flamanville sera livré à temps. Sa livraison accuse déjà six ans de retard. Mais Laurent Thieffry, directeur du projet Flamanville, se montre confiant. Selon lui, le cœur nucléaire de Flamanville "pourra battre sans problème à partir de fin 2018".</p>
+      </article>
+    }, {
+      title: "Fessenheim: une fermeture qui reste 100% politique",
+      img: 'http://static.lexpress.fr/medias_11303/w_2048,h_1146,c_crop,x_0,y_38/w_960,h_540,c_fill,g_north/v1485271918/photo-d-illustration-des-salaries-de-la-centrale-de-fessenheim-dans-une-salle-de-controle-le-14-novembre-2013_5787507.jpg',
+      author: ' Laurent Martinet',
+      category: 'Environnement',
+      source: 'Express',
+      content:
+        <article>
+          <p>Malgré l'accord donné par le conseil d'administration d'EDF au protocole d'indemnisation pour la fermeture de la centrale de Fessenheim, le processus est suspendu au résultat de l'élection présidentielle.</p>
+          <p>Inexorablement, alors que le quinquennat de François Hollande touche à sa fin, le destin de la centrale nucléaire de Fessenheim semble lui aussi scellé. Ce mardi, le conseil d'administration de son exploitant EDF a donné son accord au protocole d'indemnisation que lui propose l'Etat : 490 millions d'euros pour éteindre les réacteurs fin 2018.</p>
+          <h3>La décision du conseil d'administration, "une étape"</h3>
+          <p>Le Réseau Sortir du nucléaire se montre cependant prudent. "Cette fermeture, on l'attend depuis très longtemps, enfin elle est là. Mais tout cela reste fragile, donc on ne va pas crier victoire trop vite", a déclaré à l'AFP Charlotte Mijeon, son porte-parole. En effet, le processus de fermeture reste étroitement lié au résultat de l'élection présidentielle de mai prochain.</p>
+          <p>"La décision du conseil d'administration d'EDF n'est qu'une étape. Elle est contestable. Particulièrement à un moment si proche des élections présidentielles", assure l'association Fessenheim Notre Energie, qui regroupe les défenseurs de la centrale autour du maire de la commune, Claude Brender (UDI).</p>
+          <h3>A droite et au parti communiste, on soutient Fessenheim</h3>
+          <p>Le candidat LR François Fillon s'est prononcé contre la fermeture pendant les débats de la primaire de la droite, plaidant pour "moderniser le nucléaire." Une position à laquelle il est fidèle depuis longtemps. Pour la candidate du Front national Marine Le Pen, qui a réagi ce mardi dans un communiqué, "rien de sérieux ne peut justifier cette décision, ni la sécurité ni une quelconque stratégie énergétique ou industrielle."</p>
+          <p>A gauche, Manuel Valls soutient l'objectif de ramener la part du nucléaire à 50% de la production électrique en 2025, de même que Benoît Hamon qui veut lui substituer des énergies renouvelables.</p>
+          <p>Le candidat du Front de gauche Jean-Luc Mélenchon est partagé entre la position du parti communiste, qui dénonce la fermeture comme une "véritable aberration", et celle du parti de gauche, qui veut sortir du nucléaire. S'il est élu, il propose d'organiser un référendum pour permettre aux Français de trancher.</p>
+          <h3>Pour EDF, pas de fermeture sans Flamanville</h3>
+          <p>"Les problèmes des Français sont ailleurs", affirme pour L'Express Laurent Raynaud, ex-élu CGT de la centrale et vice-président de Fessenheim Notre Energie, qui dénonce "l'électoralisme" de la fermeture. Mais ne pense cependant pas que le thème s'imposera dans la campagne comme en 2012.</p>
+          <p>Chez EDF, on assure à L'Express que la demande d'abrogation de l'autorisation d'exploitation de la centrale, seconde étape du processus de fermeture, devrait être faite avant l'élection présidentielle. Même si elle est conditionnée à l'extension du décret de création de l'EPR de Flamanville jusqu'en 2020 et à la poursuite de l'exploitation de Paluel 2, à l'arrêt depuis plus d'un an.</p>
+          <p>Quant au décret d'abrogation de l'autorisation d'exploiter Fessenheim que le gouvernement doit prendre en guise de troisième étape, son application reste conditionnée à la mise en service de Flamanville. A moins qu'un nouveau gouvernement ne l'abroge à son tour.</p>
+        </article>
+}];
+export default prisme2;
