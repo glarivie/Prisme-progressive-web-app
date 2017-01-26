aecb465cc171e96402b9834761c138e36bcfb335
Guillaume LARIVIERE <glarivie@e2r4p5.42.fr>
2017-01-26 15:07:28 +0100

    scroll on top when mounting container Single

--- src/containers/Single/Single.js
+++ src/containers/Single/Single.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 
 import styles from './Single.css';
 import Header from '../../components/Header';
@@ -6,31 +6,41 @@ import Header from '../../components/Header';
 import prismes from '../../constants';
 import data from '../../constants/data';
 
-const Single = ({ location: { query: { article, prisme } } }) => (
-  <div className={styles.single}>
-    <Header
-      pathname={`/prisme/${prisme}`}
-      title={data[prisme].name}
-      options
-      prisme
-    />
-    <div
-      className={styles.image}
-      style={{ backgroundImage: `url('${prismes[prisme][article].img}')` }}
-    />
-    <div className={styles.content}>
-      <div className={styles.triangle} />
-      <div className={styles.text}>
-        <small>#{prismes[prisme][article].category}</small>
-        <h2>{prismes[prisme][article].title}</h2>
-        {prismes[prisme][article].content}
-      </div>
-    </div>
-  </div>
-);
+class Single extends Component {
+  static propTypes = {
+    location: PropTypes.object.isRequired,
+  };
 
-Single.propTypes = {
-  location: PropTypes.object.isRequired,
-};
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    const { location: { query: { article, prisme } } } = this.props;
+
+    return (
+      <div className={styles.single}>
+        <Header
+          pathname={`/prisme/${prisme}`}
+          title={data[prisme].name}
+          options
+          prisme
+        />
+        <div
+          className={styles.image}
+          style={{ backgroundImage: `url('${prismes[prisme][article].img}')` }}
+        />
+        <div className={styles.content}>
+          <div className={styles.triangle} />
+          <div className={styles.text}>
+            <small>#{prismes[prisme][article].category}</small>
+            <h2>{prismes[prisme][article].title}</h2>
+            {prismes[prisme][article].content}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Single;
