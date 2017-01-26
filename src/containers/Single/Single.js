2d0f35aadc8ef2a727d02d7e7d2d1c947acb1c38
Guillaume LARIVIERE <glarivie@e2r4p5.42.fr>
2017-01-26 15:50:09 +0100

    display brand and fav icon

--- src/containers/Single/Single.js
+++ src/containers/Single/Single.js
@@ -1,10 +1,14 @@
 import React, { PropTypes, Component } from 'react';
+import moment from 'moment';
 
 import styles from './Single.css';
 import Header from '../../components/Header';
 
 import prismes from '../../constants';
 import data from '../../constants/data';
+import brands from '../../constants/brands';
+import heartWhite from '../../assets/coeur_blanc.svg';
+import heartBlack from '../../assets/coeur_noir.svg';
 
 class Single extends Component {
   static propTypes = {
@@ -17,6 +21,7 @@ class Single extends Component {
 
   render() {
     const { location: { query: { article, prisme } } } = this.props;
+    const { category, title, content, source, img } = prismes[prisme][article];
 
     return (
       <div className={styles.single}>
@@ -28,14 +33,45 @@ class Single extends Component {
         />
         <div
           className={styles.image}
-          style={{ backgroundImage: `url('${prismes[prisme][article].img}')` }}
-        />
+          style={{ backgroundImage: `url('${img}')` }}
+        >
+          <img
+            src={brands[source]}
+            role="presentation"
+            alt="brand"
+            className={styles.brand}
+          />
+          <img
+            src={heartWhite}
+            role="presentation"
+            alt="heart"
+            className={styles.heart}
+          />
+        </div>
         <div className={styles.content}>
           <div className={styles.triangle} />
           <div className={styles.text}>
-            <small>#{prismes[prisme][article].category}</small>
-            <h2>{prismes[prisme][article].title}</h2>
-            {prismes[prisme][article].content}
+            <small>#{category}</small>
+            <h2>{title}</h2>
+            {content}
+          </div>
+          <div className={styles.source}>
+            <img
+              src={brands[source]}
+              role="presentation"
+              alt="brand"
+              className={styles.brand}
+            />
+            <div className={styles.credit}>
+              <small>{source} | {moment().format('DD/MM/YYYY à HH:mm')}</small>
+              <small>Par Cyrielle Bedu</small>
+            </div>
+            <img
+              src={heartBlack}
+              role="presentation"
+              alt="heart"
+              className={styles.heart}
+            />
           </div>
         </div>
       </div>
