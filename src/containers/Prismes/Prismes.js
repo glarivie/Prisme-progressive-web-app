cb0a828ce9f59feb0a61495af3053876d9ccb2df
Guillaume LARIVIERE <glarivie@e2r4p5.42.fr>
2017-01-26 12:40:51 +0100

    Fix prismes path

--- src/containers/Prismes/Prismes.js
+++ src/containers/Prismes/Prismes.js
@@ -8,8 +8,8 @@ const Prismes = () => (
   <div className={styles.prismes}>
     <Triangles kind="color" />
     <Link to="prisme/0" className={styles.top}>PRIMAIRES<br />SOCIALISTES</Link>
-    <Link to="prisme/2" className={styles.left}>CENTRALE<br />FESSENHEIM</Link>
-    <Link to="prisme/1" className={styles.down}>REVENU<br />UNIVERSEL</Link>
+    <Link to="prisme/2" className={styles.left}>REVENU<br />UNIVERSEL</Link>
+    <Link to="prisme/1" className={styles.down}>CENTRALE<br />FESSENHEIM</Link>
     {/* <Link to="prisme/3" className={styles.right}>PRISME<br />ÉCOLOGIE</Link> */}
   </div>
 );
